16cf08cea1fb1d1c057f77711276d42c3ac756dc
theosM <theos.mariani@gmail.com>
2020-02-14 15:51:13 +0100

    add test on savgins & add cli

--- src/main/java/fr/polytech/al/tfc/savings/controller/SavingsController.java
+++ src/main/java/fr/polytech/al/tfc/savings/controller/SavingsController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,9 @@ public class SavingsController {
         this.savingsObserver = savingsObserver;
     }
 
+    //604800s => 7 jours
     @PostMapping
+    @Scheduled(fixedDelay = 604800)
     public ResponseEntity<String> startComputingSavings() throws URISyntaxException, JsonProcessingException {
         savingsObserver.computeSavings();
         return new ResponseEntity<>(HttpStatus.OK);
